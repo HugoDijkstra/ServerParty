aee7ee7f4c80d0578ca28be96a6d27dc6ef04943
hugo dijkstra <hugodijkstra@live.nl>
2017-04-11 14:20:59 +0200

    Add project files.

---
+++ Server/Server.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Server
+    {
+        TcpListener listner;
+        TcpClient client;
+        public void Start()
+        {
+            listner = new TcpListener(6669);
+            listner.Start();
+            client = default(TcpClient);
+        }
+    }
+}
